9466a5412344e307b70a60188c133a1881811c81
Phillip Passmore <phillipspassmore@gmail.com>
2022-07-28 08:31:33 +0100

    first commit

---
+++ word cloud generator.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from pygame import CONTROLLER_BUTTON_Y
+from wordcloud import WordCloud, STOPWORDS
+import sys, os
+
+words = open('Corbyn Conference Speeches 2015-2019.txt', mode = 'r', encoding='utf-8').read()
+
+removed_words = STOPWORDS
+
+cloud = WordCloud(
+    mode = "RGBA",
+    background_color=None,
+    height = 600,
+    width = 400,
+    min_word_length= 4,
+    max_words= 100
+)
+
+cloud.generate(words)
+
+cloud.to_file('Corbyn Conference Speech Output.png')

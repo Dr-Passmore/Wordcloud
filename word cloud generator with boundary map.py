e0e33f7324e9619b4a2b5fe84ded1c4b394e16a4
Phillip Passmore <phillipspassmore@gmail.com>
2022-07-28 12:52:01 +0100

    stopwords added. Also shaped cloud erroring
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- word cloud generator with boundary map.py
+++ word cloud generator with boundary map.py
@@ -1,7 +1,47 @@
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 from PIL import Image
-import os
+import numpy as np
 from scipy.ndimage import gaussian_gradient_magnitude
+import os
 
+directory = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+#load text
+textfile = open(os.path.join(directory, r'Text\BA - Political Transformation and the mass media.txt'), encoding="utf-8").read()
+
+# load image
+image = np.array(Image.open(os.path.join(directory,r'Pictures\Moscow test.png')))
+
+# subsample by a factor of 3
+image = image[::3, ::3]
+
+# create mask
+image_mask = image.copy()
+print (len(image_mask.shape))
+print(image_mask)
+image_mask[image_mask.sum(axis=2) == 0] = 255
+
+#edge detection
+edges = np.mean([gaussian_gradient_magnitude(image[:, :, i] / 255., 2) for i in range(3)], axis=0)
+image_mask[edges > 0.8] = 255
+
+cloud = WordCloud(
+    mode = "RGBA",
+    background_color=None,
+    mask=image_mask,
+    height = 400,
+    width = 600,
+    min_word_length= 4,
+    max_words= 200
+)
+cloud.generate(textfile)
+
+image_colours = ImageColorGenerator(image)
+cloud.recolor(color_func=image)
+plt.figure(figsize=(10, 10))
+plt.imshow(cloud, interpolation="bilinear")
+
+
+output = r'WordClouds\undergrad Dissertation.png' 
+cloud.to_file(output)
 
--- word cloud generator.py
+++ word cloud generator.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-textfile = 'Text\Journalistic norms.txt'
+textfile = r'Text\Corbyn Conference Speeches 2015-2019.txt'
 
-output = 'WordClouds\Masters Dissertation.png' 
+output = r'WordClouds\Corbyn Conference Speech Output.png' 
 
 words = open(textfile, mode = 'r', encoding='utf-8').read()
 
@@ -12,9 +12,10 @@ removed_words = STOPWORDS
 cloud = WordCloud(
     mode = "RGBA",
     background_color=None,
+    stopwords=removed_words,
     height = 600,
     width = 400,
-    min_word_length= 4,
+    min_word_length= 3,
     max_words= 200
 )
 

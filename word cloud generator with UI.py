8c28aa8cf9541d5dc19eb567c9d7f32aed87eb1d
Phillip Passmore <phillipspassmore@gmail.com>
2022-07-29 08:11:09 +0100

    Starting a UI version
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

---
+++ word cloud generator with UI.py
@@ -0,0 +1,22 @@
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from scipy.ndimage import gaussian_gradient_magnitude
+import os
+import tkinter as tk
+
+class wordcloud:
+    def __init__(self):
+        print("hello world")
+        
+    def selectImage(self):
+    
+    def selectText(self):
+    
+    def cloudShape(Self):
+        
+    def generateCloud(self):
+        
+        
+wordcloud()

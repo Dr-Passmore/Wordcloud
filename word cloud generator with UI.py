cc0d529de0a42e9b57560a202df10fdafc4b52df
Phillip Passmore <phillipspassmore@gmail.com>
2022-09-01 16:45:40 +0100

    AB#17 UI resizable by user
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- word cloud generator with UI.py
+++ word cloud generator with UI.py
@@ -120,7 +120,6 @@ class wordCloud:
         self.colourSet5 = ['twilight', 'twilight_shifted', 'hsv']
         self.colourSet6 = ['Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2', 'Set1', 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b', 'tab20c']
         self.colourSet7 = ['flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet', 'turbo', 'nipy_spectral', 'gist_ncar']
-        
         self.colourType = ttk.Combobox(self.root, 
             value = Types, 
             state="readonly",
@@ -216,6 +215,7 @@ class wordCloud:
         button_save.pack()
         button_reset.pack()
         button_exit.pack()
+        self.root.resizable(True, True)
         self.root.mainloop()
         
     def pickColour(event, self):

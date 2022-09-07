4759f914f33a62c63f6ecdd703fa18ae03034dc4
Phillip Passmore <phillipspassmore@gmail.com>
2022-09-07 10:16:31 +0100

    AB#61 Recolouring checks canvas size
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- WordCloud Generator.py
+++ WordCloud Generator.py
@@ -320,32 +320,35 @@ class wordCloud:
         else:
             self.selectedImage.config(text = "Image selection is \n{}".format(tail))
             logging.info("The selected image is {}".format(tail))
-            im = np.array(Image.open(os.path.join(self.Image)))
-            updated = False
             self.widthInput.delete(0, 'end')
             self.heightInput.delete(0, 'end')
             self.heightInput.insert(0, 1280)
             self.widthInput.insert(0, 1920)
-            width = int(self.widthInput.get())
-            height = int(self.heightInput.get())
-            logging.info("Checking image width - {} - and image height - {}".format(width, height))
-            if im.shape[1] < width:
-                self.widthInput.delete(0, 'end')
-                self.widthInput.insert(0, im.shape[1])
-                logging.warning("Image selected width is smaller than current canvas. Resizing to {}".format(im.shape[1]))
-                updated = True
-            if im.shape[0] < height:
-                self.heightInput.delete(0, 'end')
-                self.heightInput.insert(0, im.shape[0])
-                logging.warning("Image selected height is smaller than current canvas. Resizing to {}".format(im.shape[0]))
-                updated = True
-            if updated == True:
-                logging.warning("Canvas resized")
-                messagebox.showerror("Canvas Resized", "Canvas has been resized due to image selected. \nIf you wish to recolour using image you cannot increase the canvas size")
-            else: 
-                logging.info("Image selected fits the current canvas settings")
+            wordCloud.imageCanvasCheck(self)
             return self.Image
-        
+    
+    def imageCanvasCheck(self):
+        im = np.array(Image.open(os.path.join(self.Image)))
+        updated = False
+        width = int(self.widthInput.get())
+        height = int(self.heightInput.get())
+        logging.info("Checking image width - {} - and image height - {}".format(width, height))
+        if im.shape[1] < width:
+            self.widthInput.delete(0, 'end')
+            self.widthInput.insert(0, im.shape[1])
+            logging.warning("Image selected width is smaller than current canvas. Resizing to {}".format(im.shape[1]))
+            updated = True
+        if im.shape[0] < height:
+            self.heightInput.delete(0, 'end')
+            self.heightInput.insert(0, im.shape[0])
+            logging.warning("Image selected height is smaller than current canvas. Resizing to {}".format(im.shape[0]))
+            updated = True
+        if updated == True:
+            logging.warning("Canvas resized")
+            messagebox.showerror("Canvas Resized", "Canvas has been resized due to image selected. If you wish to recolour text using the image you cannot increase the canvas size")            
+        else: 
+            logging.info("Image selected fits the current canvas settings")
+    
     def selectText(self):
         self.selectFile = filedialog.askopenfilename(
             initialdir = r"/Text/",
@@ -398,7 +401,6 @@ class wordCloud:
             self.repeat = False
         logging.info("Include repeated words is set to {}".format(self.repeat))
         return self.numberTicked
-        
     
     def cloudShape(self):
         if self.Image == "":
@@ -419,6 +421,7 @@ class wordCloud:
             logging.error('No .png file selected, Generating WordCloud Without Image Settings')
             messagebox.showerror("Image File Error", "No Image File Selected, Generating WordCloud Without Image Settings")
         else:
+            wordCloud.imageCanvasCheck(self)
             self.newImage = np.array(Image.open(os.path.join(self.Image)))
             self.image_colours = ImageColorGenerator(self.newImage)
             self.textColourChange = True
